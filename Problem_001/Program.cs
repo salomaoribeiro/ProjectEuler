31ed7de8c4c11a6b9d834369d1ea14bf1d358e57
Salomão Ribeiro <salomaomdm@gmail.com>
2022-08-31 14:40:53 -0300

    Pull Request Adjustment

--- Problem_001/Program.cs
+++ Problem_001/Program.cs
@@ -1,12 +1,4 @@
-﻿//var result = 0;
-long target = 1_000_000_000;
-
-//for (int i = 0; i < 1000; i++)
-//{
- //   if (i % 3 == 0 || i % 5 == 0)
-//      result += i;
-//}
-
+﻿long target = 1_000_000_000;
 
 long SumDivideBy(long number)
 {
@@ -14,8 +6,6 @@ long SumDivideBy(long number)
   long teste = number * (p * (p+1)) / 2;
   return teste;
 }
-//System.Console.WriteLine(result);
+
 System.Console.WriteLine(SumDivideBy(3) + SumDivideBy(5) - SumDivideBy(15));
-System.Console.WriteLine(SumDivideBy(3)); 
-System.Console.WriteLine(SumDivideBy(5)); 
-System.Console.WriteLine(SumDivideBy(15));
+
