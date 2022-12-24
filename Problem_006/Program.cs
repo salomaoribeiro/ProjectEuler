e050344f5eeec3f655e209a22b7aede0cf52967d
Salomão Ribeiro <salomaomdm@gmail.com>
2022-12-24 14:07:57 -0300

    Ajuste - Acrescentada solução do site

--- Problem_006/Program.cs
+++ Problem_006/Program.cs
@@ -1,7 +1,19 @@
-﻿int soma = 0, quadrado = 0;
+﻿// Minha solução
+int soma = 0, quadrado = 0;
 for (int i = 1; i <= 100; i++)
 {
     quadrado += i * i;
     soma += i;
 }
-Console.WriteLine($"Resultado: {(soma * soma) - quadrado}");
+Console.WriteLine($"Soma2  {soma * soma}");
+Console.WriteLine($"Soma Quadrados  {quadrado}");
+Console.WriteLine($"Resultado: {(soma * soma) - quadrado}\n\n");
+
+// Solução do site
+var limite = 100;
+var soma2 = limite * (limite + 1) / 2;
+var somaQuadrados = (2 * limite + 1) * (limite + 1) * limite / 6;
+
+Console.WriteLine($"Soma2  {soma2 * soma2}");
+Console.WriteLine($"Soma Quadrados  {somaQuadrados}");
+Console.WriteLine(soma2 * soma2 - somaQuadrados);
